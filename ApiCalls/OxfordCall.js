03ece6cd133b72d1284bd87b1f37e1de3c93d9be
Hugo Mora <>
2021-09-13 11:13:45 -0500

    add oxford api call #4

--- ApiCalls/OxfordCall.js
+++ ApiCalls/OxfordCall.js
@@ -1,26 +1,49 @@
-function getOxfordApiTest() {
-  const app_id = '5197b755';
-  const app_key = 'db4d018579f1b488ab2167d66cd2ce72';
-  const language = "en-us";
-  var word_id = 'hello';
-  var url = 'https://od-api.oxforddictionaries.com:443/api/v2/entries/' + language + '/' + word_id.toLowerCase() + "?fields=definitions&strictMatch=false";
+const cors = require('cors');
+const express = require('express');
+const http = require("https");
 
-  var request = new XMLHttpRequest();
+const app = express();
+app.use(cors());
+app.options('*', cors());
 
-  request.open('GET', url, true);
-  request.setRequestHeader('app_id', app_id); 
-  request.setRequestHeader('app_key', app_key);
-  request.setRequestHeader('Access-Control-Allow-Origin', '*');
-  request.setRequestHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  request.setRequestHeader('Access-Contorl-Allow-Methods', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
+const port = 8080;
 
+const app_id = "5197b755"; // insert your APP Id
+const app_key = "db4d018579f1b488ab2167d66cd2ce72"; // insert your APP Key
+const fields = "definitions,pronunciations";
+const strictMatch = "false";
 
-  request.onload = function() {
-      //Begin accessing JSON data here
+app.get('/', (req, res, next) => {
+  res.send('welcome to my API...');
+});
 
-      var data = JSON.parse(this.response);
-      console.log(data);
-  }
+app.get('/definitions', (req, res, next) => {
+  var word = req.query.word;
+  console.log(word);
+  var options = {
+    host: 'od-api.oxforddictionaries.com',
+    port: '443',
+    path: '/api/v2/entries/en-gb/' + word + '?fields=' + fields + '&strictMatch=' + strictMatch,
+    method: "GET",
+    headers: {
+      'app_id': app_id,
+      'app_key': app_key
+    }
+  };
 
-  request.send();
-}
+  http.get(options, (resp) => {
+      let body = '';
+      resp.on('data', (d) => {
+          body += d;
+      });
+      resp.on('end', () => {
+          const obj = JSON.parse(body);
+          
+          res.send(obj);
+      });
+  });
+});
+
+app.listen(port, () => {
+  console.log('lsitening on port: ' + port);
+});
